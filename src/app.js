d50e7728b7d25402f4d371478c9f31e8458dc3c2
Harish Yerra <harish.yerra@duke.edu>
2020-04-02 02:25:10 -0500

    Create Project model.

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const { User } = require('./db/User');
 const { Job } = require('./db/Job');
+const { Project } = require('./db/Project');
 
-const res = User.login('test@test.com');
-res.then(user => {
-    user.fetchDetails().then(() => {
-        console.log(user);
-    });
-});
+// const res = User.login('test@test.com');
+// res.then(user => {
+//     user.fetchDetails().then(() => {
+//         console.log(user);
+//     });
+// });
+//
+// Job.getAllJobs().then(jobs => {
+//     console.log(jobs);
+// });
 
-Job.getAllJobs().then(jobs => {
-    console.log(jobs);
+Project.getAllProjects().then(projects => {
+    console.log(projects);
 });
---
+++ src/db/Project.js
@@ -0,0 +1,39 @@
+const db = require('./db');
+
+class Project {
+    constructor(id, title, description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+    async fetchDetails() {
+        const query = `SELECT * FROM Project WHERE id = '${this.id}';`;
+        try {
+            const result = db.executeQuery(query);
+            const projectResult = result[0];
+            this.title = projectResult.title;
+            this.description = projectResult.description;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    static async getAllProjects() {
+        const query = `SELECT * FROM Project;`;
+        const projects = [];
+        try {
+            const results = await db.executeQuery(query);
+            results.forEach(project => {
+                projects.push(new Project(project.id, project.title, project.description));
+            });
+            return projects;
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+
+module.exports = {
+    Project
+};
