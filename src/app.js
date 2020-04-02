b5497c9da0a886f7524811cfc4a0830056ee555f
Harish Yerra <harish.yerra@duke.edu>
2020-04-02 13:54:07 -0500

    Make sure to use CORS.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cors = require('cors');
+
 const application = require('./routes/application');
 const job = require('./routes/job');
 const project = require('./routes/project');
@@ -7,6 +9,7 @@ const user = require('./routes/user');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use('/application', application);
 app.use('/jobs', job);
 app.use('/project', project);
