92480323319bb19d2fdf5a3c9639f94acf9f7146
Harish Yerra <harish.yerra@duke.edu>
2020-04-24 19:19:37 -0500

    Setup session support.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const session = require('express-session');
 const cors = require('cors');
 const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
 const application = require('./routes/application');
 const job = require('./routes/job');
@@ -13,6 +16,26 @@ const { User } = require('./db/User');
 const app = express();
 const port = process.env.PORT || 5000;
 
+passport.use(strategy);
+
+passport.serializeUser(User.serializeUser);
+passport.deserializeUser(User.deserializeUser);
+
+app.use(cookieParser());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.set('trust proxy', 1);
+app.use(session({
+    secret: 'dskjfoiewjaomofaicu8eurowijocijziosu8fwjonsoifojewofiegho',
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(cors());
 app.use('/api/application', application);
 app.use('/api/job', job);
@@ -20,18 +43,13 @@ app.use('/api/project', project);
 app.use('/api/user', user);
 app.post('/login', (req, res, next) => {
     passport.authenticate('local', (error, user, info) => {
-        if (error) return res.send({ error: error.message });
-        if (!user) return res.send({ error: 'Wrong username or password.' });
-        res.send(user);
+        req.logIn(user, function (loginError) {
+            if (error) return res.send({ error: error.message });
+            if (loginError) return res.send({ error: error.message });
+            if (!user) return res.send({ error: 'Wrong username or password.' });
+            res.send(user);
+        });
     })(req, res, next);
 });
-
-passport.use(strategy);
-
-passport.serializeUser(User.serializeUser);
-passport.deserializeUser(User.deserializeUser);
-
-app.use(passport.initialize());
-app.use(passport.session());
 
 app.listen(port);
