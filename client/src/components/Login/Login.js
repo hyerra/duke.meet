bdea72aeef5be55248b167fc98702b3801b800ea
Harish Yerra <harish.yerra@duke.edu>
2020-04-25 16:31:08 -0500

    Utilize Semantic UI Form elements

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,52 +1,29 @@
-import React from "react";
-import { Dropdown } from "semantic-ui-react";
-import { Image } from "semantic-ui-react";
+import React from 'react';
+import { Form, Image } from 'semantic-ui-react';
+import loginImage from '../images/025.png';
 
-// const RegisterImage = () => (
-//     <Image src='026.png' size='small' />
-//   );
-
-const Login = (props) => {
-  return (
-    <div
-      style={{
-        marginTop: "5rem",
-        marginBottom: "5rem",
-        display: "flex",
-        width: "100%"
-      }}
-    >
-      <div style={{ width: "30%" , paddingTop: "4rem",}}>
-        <h1>Login</h1>
-        <form style={{ paddingTop: "2rem" }}>
-          <label>Email</label>
-          <div class="ui fluid input" style={{ paddingTop: "0.75rem" }}>
-            <input type="text" placeholder="example@example.com" />
-          </div>
-
-          <br />
-          <br />
-
-          <label>Password</label>
-          <div class="ui fluid input" style={{ paddingTop: "0.75rem" }}>
-            <input type="text" placeholder="*******" />
-          </div>
-
-          <div
-            class="field"
-            style={{ paddingTop: "4rem", paddingBottom: "4rem" }}
-          >
-            <button class="ui button">Login</button>
-          </div>
-        </form>
-      </div>
-      <div style={{ width: "20%" }}>
-      </div>
-      <div style={{ width: "50%", paddingTop: "0rem"}} class="ui centered image">
-        <Image class="ui centered image" src={require("./images/025.png")} size="large" />
-      </div>
+const Login = () => (
+  <div
+    style={{
+      marginTop: '5rem',
+      marginBottom: '5rem',
+      display: 'flex',
+      width: '100%',
+    }}
+  >
+    <div style={{ width: '30%', paddingTop: '4rem' }}>
+      <h1>Login</h1>
+      <Form style={{ paddingTop: '2rem' }}>
+        <Form.Input label="Email" type="email" placeholder="example@example.com" />
+        <Form.Input label="Password" type="password" placeholder="*******" />
+        <Form.Button>Login</Form.Button>
+      </Form>
     </div>
-  );
-};
+    <div style={{ width: '20%' }} />
+    <div style={{ width: '50%', paddingTop: '0rem' }} className="ui centered image">
+      <Image class="ui centered image" src={loginImage} size="large" />
+    </div>
+  </div>
+);
 
 export default Login;
--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,111 +1,65 @@
-import React from "react";
-import { Dropdown } from "semantic-ui-react";
-import { Image } from "semantic-ui-react";
-
-// const RegisterImage = () => (
-//     <Image src='026.png' size='small' />
-//   );
+import React from 'react';
+import { Form, Image } from 'semantic-ui-react';
+import registerImage from '../images/026.png';
 
 const options = [
   {
-    key: "2020",
-    text: "2020",
-    value: "2020",
+    key: '2020',
+    text: '2020',
+    value: '2020',
   },
   {
-    key: "2021",
-    text: "2021",
-    value: "2021",
+    key: '2021',
+    text: '2021',
+    value: '2021',
   },
   {
-    key: "2022",
-    text: "2022",
-    value: "2022",
+    key: '2022',
+    text: '2022',
+    value: '2022',
   },
   {
-    key: "2023",
-    text: "2023",
-    value: "2023",
+    key: '2023',
+    text: '2023',
+    value: '2023',
   },
   {
-    key: "2024",
-    text: "2024",
-    value: "2024",
+    key: '2024',
+    text: '2024',
+    value: '2024',
   },
 ];
 
-const Register = (props) => {
-  return (
-    <div
-      style={{
-        marginTop: "3rem",
-        marginBottom: "5rem",
-        display: "flex",
-        width: "100%"
-      }}
-    >
-      <div style={{ width: "30%" , paddingTop: "2rem",}}>
-        <h1>Sign Up</h1>
-        <form style={{ paddingTop: "2rem" }}>
-          <label>Name</label>
-          <div class="ui fluid input" style={{ paddingTop: "0.75rem" }}>
-            <input type="text" placeholder="Jane Doe" />
-          </div>
-
-          <br />
-          <br />
-
-          <label>Email</label>
-          <div class="ui fluid input" style={{ paddingTop: "0.75rem" }}>
-            <input type="text" placeholder="example@example.com" />
-          </div>
-
-          <br />
-          <br />
-
-          <label>Password</label>
-          <div class="ui fluid input" style={{ paddingTop: "0.75rem" }}>
-            <input type="text" placeholder="*******" />
-          </div>
-
-          <br />
-          <br />
-          <div style={{ display: "flex" }}>
-            <div style={{ width: "35%" }}>
-              <label>Year</label>
-              <div style={{ paddingTop: "0.75rem" }}>
-                <Dropdown
-                  placeholder="Year"
-                  fluid
-                  selection
-                  options={options}
-                />
-              </div>
-            </div>
-
-            <div style={{ width: "65%", paddingLeft: "3rem" }}>
-              <label>Major</label>
-              <div class="ui fluid input" style={{ paddingTop: "0.75rem" }}>
-                <input type="text" placeholder="Major" />
-              </div>
-            </div>
-          </div>
-
-          <div
-            class="field"
-            style={{ paddingTop: "4rem", paddingBottom: "4rem" }}
-          >
-            <button class="ui button">Sign Up</button>
-          </div>
-        </form>
-      </div>
-      <div style={{ width: "22%" }}>
-      </div>
-      <div style={{ width: "48%", paddingTop: "4rem"}} class="ui centered image">
-        <Image class="ui centered image" src={require("./images/026.png")} size="large" />
-      </div>
+const Register = () => (
+  <div
+    style={{
+      marginTop: '3rem',
+      marginBottom: '5rem',
+      display: 'flex',
+      width: '100%',
+    }}
+  >
+    <div style={{ width: '30%', paddingTop: '2rem' }}>
+      <h1>Sign Up</h1>
+      <Form style={{ paddingTop: '2rem' }}>
+        <Form.Input label="Name" type="text" placeholder="John Doe" />
+        <Form.Input label="Email" type="email" placeholder="example@example.com" />
+        <Form.Input label="Password" type="password" placeholder="*******" />
+        <Form.Select
+          fluid
+          label="Year"
+          options={options}
+          placeholder="Year"
+        />
+        <Form.Input label="Major" type="text" placeholder="Major" />
+        <Form.Button style={{ marginBottom: '4rem' }}>Sign Up</Form.Button>
+      </Form>
     </div>
-  );
-};
+    <div style={{ width: '22%' }} />
+    <div style={{ width: '48%', paddingTop: '4rem' }} className="ui centered image">
+      <Image class="ui centered image" src={registerImage} size="large" />
+    </div>
+  </div>
+);
 
 export default Register;
