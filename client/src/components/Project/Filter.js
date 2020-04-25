2dcb8f92ee02198ca240215a17309e972a38b149
Harish Yerra <harish.yerra@duke.edu>
2020-04-25 18:58:15 -0500

    Fetch skills in client.

--- client/src/components/Project/Filter.js
+++ client/src/components/Project/Filter.js
@@ -1,36 +1,30 @@
 import React from 'react';
 import { Dropdown } from 'semantic-ui-react';
+import Skill  from './../../model/Skill'
+import skill  from './../../api/skill';
 
+class DropdownSkillsSelection extends React.Component {
+  state = { skills: [] };
 
-const options = [
-  { key: 'SQL', text: 'SQL', value: 'SQL' },
-  { key: 'Java', text: 'Java', value: 'Java' },
-  { key: 'Python', text: 'Python', value: 'Python' },
-  { key: 'Web Development', text: 'Web Development', value: 'Web Development' },
-  { key: 'C', text: 'C', value: 'C' },
-  { key: 'C++', text: 'C++', value: 'C++' },
-  { key: 'C#', text: 'C#', value: 'C#' },
-  { key: 'React', text: 'React', value: 'React' },
-  { key: 'Node.js', text: 'Node.js', value: 'Node.js' },
-  { key: 'Objective-C', text: 'Objective-C', value: 'Objective-C' },
-  { key: 'UX Design', text: 'UX Design', value: 'UX Design' },
-  { key: 'HTML', text: 'HTML', value: 'HTML' },
-  { key: 'CSS', text: 'CSS', value: 'CSS' },
-  { key: 'Javascript', text: 'Javascript', value: 'Javascript' },
-  { key: 'PHP', text: 'PHP', value: 'PHP' },
-  { key: 'Perl', text: 'Perl', value: 'Perl' },
-  { key: 'Ruby', text: 'Ruby', value: 'Ruby' },
-  { key: 'XML', text: 'XML', value: 'XML' },
-  { key: 'Swift', text: 'Swift', value: 'Swift' },
-  { key: 'Android SDK', text: 'Android SDK', value: 'Android SDK' },
-  { key: 'Product Management', text: 'Product Management', value: 'Product Management' },
-  { key: 'Angular', text: 'Angular', value: 'Angular' },
-  { key: 'Flask', text: 'Flask', value: 'Flask' },
-  { key: 'Git', text: 'Git', value: 'Git' },
-];
+  componentDidMount() {
+    this.fetchSkills();
+  }
 
-const DropdownExampleMultipleSelection = () => (
-  <Dropdown placeholder="Filter by skills" fluid multiple selection options={options} />
-);
+  async fetchSkills() {
+    try {
+      const skillResponse = await skill.get('/');
+      console.log(skillResponse);
+      const skills = skillResponse.data.map((skillData) => new Skill(skillData.id, skillData.name));
+      this.setState({ skills });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-export default DropdownExampleMultipleSelection;
+  render() {
+    const options = this.state.skills.map(skill => { return { key: skill.id, text: skill.name, value: skill.name } });
+    return <Dropdown placeholder="Filter by skills" fluid multiple selection options={options}/>
+  }
+};
+
+export default DropdownSkillsSelection;
