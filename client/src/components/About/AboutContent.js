4d5f11bdb6999f10d1f60ddf4fdd0a7481cf8593
aa399 <31966131+aa399@users.noreply.github.com>
2020-04-25 16:25:09 -0400

    About description
    added more of a description to the about page

--- client/src/components/About/AboutContent.js
+++ client/src/components/About/AboutContent.js
@@ -6,10 +6,21 @@ const AboutContent = () => (
   <div>
     <h1 className="AboutContent">About us</h1>
     <br />
-    <p>
+    <p className ="AboutDescription">
       {' '}
       Our goal is to streamline the process of connecting top talent to
       innovative startups.
+    </p>
+    <br />
+    <p className = 'MoreAbout'> 
+      duke.meet was started by 5 Duke students enrolled in CompSci 316 who 
+      were interested in creating a platform for crowdsourcing top University
+      talent. They felt there was a gap in the ability for projects, and startups, to 
+      advertise to University talent in a simple and concise way, and decided to 
+      do something to change that. duke.meet combines this ease of posting and advertising
+      projects, with being a space for interested students to see what opportunities are 
+      out there. Whether it be a class project or a startup idea, duke.meet aims to be the 
+      go to platform for connecting University talent to exciting projects!
     </p>
   </div>
 );
