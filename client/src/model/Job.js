f51a7277459cdfc937700de29ee852764bf164d2
micheal <mth36@duke.edu>
2020-04-24 23:42:20 -0500

    design basic jobs page template with example data

---
+++ client/src/model/Job.js
@@ -0,0 +1,11 @@
+class Job {
+  constructor(projectId, id, title, payment, timeCommitment) {
+    this.projectId = projectId;
+    this.id = id;
+    this.title = title;
+    this.payment = payment;
+    this.timeCommitment = timeCommitment;
+  }
+}
+
+export default Job
