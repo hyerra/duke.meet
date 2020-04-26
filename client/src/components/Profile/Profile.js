42c1e8122863956bc121750754a524e39336f507
micheal <mth36@duke.edu>
2020-04-26 00:35:07 -0500

    restructured Profile.js

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,48 +1,73 @@
 import React from 'react';
-import {List, Card, Button} from 'semantic-ui-react';
+import {List, Grid, Card, Button, GridColumn, CardGroup} from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 class Profile extends React.Component {
     render() {
         return (
-    <div>
-        <Card fluid header='Name' />
-        <br /><br />
+            <div>
+                <Card fluid header='Name' />
+
+                <br /><br />
+
+                <ProfileInfo/>
+
+                <br /><br />
+
+                <ProfileProjects/>
+
+                <ProfileJobs/>
+            </div>
+        );
+    }
+}
+
+const ProfileInfo = () => {
+    return (
         <Card centered>
             <Card.Content>
                 <Card.Header>Major</Card.Header>
                 <Card.Meta>Email</Card.Meta>
-                <Card.Description>
-                    Year
-                </Card.Description>
+                <Card.Description>Year</Card.Description>
             </Card.Content>
         </Card>
-        <br /><br />
-        <Card fluid header='My Projects' />
+    );
+}
 
-    <List>
-        <List.Item> 
-            <Button content='Edit *Project 1*' animated as={Link} 
-            to={'/ProjectEdit'} /> 
-            <br /> <br />
-            <List.Item> <Button content = 'Add Project' animated as={Link}
-            to={'ProjectEdit'}/></List.Item>
-        </List.Item>
-    </List>  
+const ProfileProjects = ({projects}) => {
+    return (
+        <div>
+            <Card fluid header='My Projects' />
 
-    <Card fluid header='My Jobs' />
-    <List>
-        <List.Item> 
-            <Button content='Edit -job 1- 'size='tiny' animated as={Link} 
-            to={'/JobEdit'} /> 
-            <br /> <br />
-            <List.Item> <Button content = 'Add Job' animated as={Link}
-            to={'JobEdit'}/></List.Item>
-        </List.Item>
-    </List>
-    </div>
-        );
-    }
+            <List>
+                <List.Item>
+                    <CardGroup>
+
+                    </CardGroup>
+                    <List.Item> <Button content='Add Project' animated as={Link}
+                        to={'ProjectEdit'} /></List.Item>
+                </List.Item>
+            </List>
+        </div>
+    );
+}
+
+const ProfileJobs = () => {
+    return (
+        <div>
+            <Card fluid header='My Jobs' />
+
+            <List>
+                <List.Item>
+                    <Button content='Edit -job 1- ' size='tiny' animated as={Link}
+                        to={'/JobEdit'} />
+                    <br /> <br />
+                    <List.Item> <Button content='Add Job' animated as={Link}
+                        to={'JobEdit'} /></List.Item>
+                </List.Item>
+            </List>
+        </div>
+    );
 }
 
 export default Profile;
